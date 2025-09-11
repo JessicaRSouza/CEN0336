56f90897b06ca7b16d6466ba2bbc531ff86e2308
Jessica Souza <jessica.souza0408@usp.br>
2025-09-11 15:24:19 -0300

    Estudando

---
+++ Aulas/Python1/amigos.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys
+
+amigo1 = sys.argv[1] #parâmetro 1
+amigo2 = sys.argv[2] #parâmetro 2
+
+print(amigo1, "e", amigo2, "são amigos") #mensagem a ser exibida na tela
